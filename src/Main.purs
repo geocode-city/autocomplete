76c6768088ac131836792d0e14961a108434244b
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-01-30 17:52:28 -0500

    More configuration, events, minor markup updates
    * Don't make a request if the search term is empty
    * Config type, some initial config entries
    * Use inputClass and inputName from config
    * introduce a powered-by element
    * Add events for results: citiesFound, noCitiesFound.

--- src/Main.purs
+++ src/Main.purs
@@ -6,13 +6,14 @@ import Autocomplete.Component as Autocomplete
 import Autocomplete.Events as Events
 import Control.Coroutine as CR
 import Data.Foldable (for_)
-import Data.Maybe (Maybe(..))
+import Data.Int (fromString)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Effect (Effect)
 import Effect.Class.Console (log)
 import Halogen (liftEffect)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-import Web.DOM.Element (getAttribute, toEventTarget, toNode)
+import Web.DOM.Element (Element, getAttribute, toEventTarget, toNode)
 import Web.DOM.Node (Node, childNodes, removeChild)
 import Web.DOM.NodeList (toArray)
 import Web.DOM.ParentNode (QuerySelector(..))
@@ -30,11 +31,17 @@ main = HA.runHalogenAff do
       -- clear out all children of the container.
       let asElement = toElement el
       _ <- liftEffect $ removeChildren (toNode asElement)
-      apiKey <- liftEffect $ getAttribute "data-api-key" asElement
-      io <- runUI (Autocomplete.component apiKey) unit el
+      cfg <- liftEffect $ extractConfig asElement
+      io <- runUI Autocomplete.component cfg el
       let evtTarget = toEventTarget $ asElement
       io.subscribe $ CR.consumer \msg ->
         case msg of
+          (Autocomplete.ResultsFound cities) -> do
+            _ <- liftEffect $ dispatchEvent (toEvent $ Events.citiesFound cities) evtTarget
+            pure Nothing
+          Autocomplete.NoResults -> do
+            _ <- liftEffect $ dispatchEvent (toEvent $ Events.noCitiesFound) evtTarget
+            pure Nothing
           (Autocomplete.Selected city) -> do
             _ <- liftEffect $ dispatchEvent (toEvent $ Events.citySelected city) evtTarget
             --liftEffect $ log $ "Selected: " <> city.name
@@ -49,3 +56,13 @@ removeChildren :: Node -> Effect Unit
 removeChildren parent = do
   children <- childNodes parent >>= toArray
   for_ children (\child -> removeChild child parent)
+
+extractConfig :: Element -> Effect Autocomplete.Config
+extractConfig element = do
+  apiKey <- getAttribute "data-api-key" element
+  inputName <- getAttribute "data-input-name" element
+  inputClass <- getAttribute "data-input-class" element
+  -- the API defaults to 5
+  suggestionCountStr <-  getAttribute "data-suggestion-count" element
+  let suggestionCount = fromMaybe Nothing $ fromString <$> suggestionCountStr
+  pure $ {apiKey, inputName, inputClass, suggestionCount}
